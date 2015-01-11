2cccc7d1e2771ebc4c9eb2437ecb56ff5f74d29d
zaaras <zaara.syeda@gmail.com>
2015-01-11 16:07:41 -0500

    remove outliers function

--- algo_api.py
+++ algo_api.py
@@ -2,6 +2,11 @@ from flask import Flask, url_for
 from flask import json
 from flask import request
 
+import numpy as np
+import sys
+import matplotlib.pyplot as pyplot
+import pylab as pl
+
 app = Flask(__name__)
 
 delta = [];
@@ -15,6 +20,7 @@ theta = [];
 time = [];
 attention_native = [];
 meditation_native = [];
+waves = [];
 
 @app.route('/')
 def api_root():
@@ -37,11 +43,64 @@ def api_post_waves():
 		theta.append((d[1])['wave7'])
 		attention_native.append((d[1])['attention'])
 		meditation_native.append((d[1])['meditation'])
+
+
+	waves.append(delta)
+	waves.append(high_alpha)
+	waves.append(high_beta)
+	waves.append(low_alpha)
+	waves.append(low_beta)
+	waves.append(low_gamma)
+	waves.append(mid_gamma)
+	waves.append(theta)
+	
+	remove_outliers()
+	#standardize(waves)
+	#average(waves)
+	#aggregate(waves)
+
 	return json.dumps(data)
 
 @app.route('/get_results', methods = ['GET'])
 def api_get_results():
 	return time
+
+def remove_outliers():
+	outliers = []
+	for wave in waves:
+		for i in range (0,(len(wave)-1)):
+			if (wave[i] > 1400000):
+				del time[i]
+				outliers.append(i)
+	
+	for outlier in outliers:
+		for wave in waves:
+			del wave[outlier]
+
+	return
+
+def average(wave,time_stamps, window) :
+        avg_wave = [];
+        avg_wave_time = [];
+        size = len(wave);
+        times = size/window;
+        print size;
+        print times;
+        i = 0;
+        j=0;
+        count = 0;
+        sum = 0;
+        for j in range(times):
+                avg_wave_time.append(time_stamps[(i+i+window)/2]);
+                while(count<window):
+                        sum = sum + wave[i];
+                        i=i+1;
+                        count = count +1;
+                avg_wave.append(sum/window);
+                sum = 0;
+                count = 0;
+
+        return [avg_wave, avg_wave_time];
 
 if __name__ == '__main__':
 	app.debug = True
